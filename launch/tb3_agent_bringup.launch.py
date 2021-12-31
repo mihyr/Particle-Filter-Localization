ec1c1a7bc59f1e7ebf8090740aebe27f24063e77
whomihirpatel <mspatel1298@gmail.com>
2021-12-31 10:33:19 -0600

    added agent launch file

--- launch/tb3_agent_bringup.launch.py
+++ launch/tb3_agent_bringup.launch.py
@@ -1,3 +1,6 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -7,16 +10,19 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution, TextSubstitution
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
+    tb3_bringup = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         get_package_share_directory('turtlebot3_bringup'), 'launch'),
+         '/robot.launch.py'])
+      )
+
+    pf_landmark_detector =  Node(
             package='particle_filter',
             executable='landmark_detector.py',
             name='landmark_detector'
-        ),
-        Node(
-            package='turtlesim',
-            namespace='turtlesim2',
-            executable='turtlesim_node',
-            name='sim'
         )
+
+    return LaunchDescription([
+        
+        pf_landmark_detector
     ])
